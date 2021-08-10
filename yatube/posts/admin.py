32c9702b944ce3df505373f1bd345fe5421f88ff
vovanbart <vovanbart@icloud.com>
2021-08-11 02:23:46 +0300

    Проект

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -16,8 +16,10 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
+
 # При регистрации модели Post источником конфигурации для неё назначаем
 # класс PostAdmin
-admin.site.register(Post, PostAdmin)  
+admin.site.register(Post, PostAdmin)
+
 
 admin.site.register(Group)
