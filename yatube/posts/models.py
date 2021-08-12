bb91c44def1f864ff9db46f9546b277d86dc669c
vovanbart <vovanbart@icloud.com>
2021-08-12 10:50:18 +0300

    Проект

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        # related_name='posts'
+        related_name='posts'
     )
     group = models.ForeignKey(
         Group,
@@ -27,8 +27,8 @@ class Post(models.Model):
         related_name='posts',
         blank=True, null=True)
 
-    def __str__(self):
-        return self.text
-
     class Meta:
         ordering = ('-pub_date', )
+
+    def __str__(self):
+        return self.text[:15]
